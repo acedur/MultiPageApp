90d0e625b3ba0b48fa679a1657c2579a80fccb53
Aleksandar Duranovski <aduranovski@gmail.com>
2022-12-18 00:12:07 +0100

    edit

--- src/Components/Cards/Card/index.js
+++ src/Components/Cards/Card/index.js
@@ -1,24 +1,30 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./index.css";
 
 function Card({ data, singleProduct = false }) {
   const { title, image, price, id, description } = data;
+  let navigate = useNavigate();
   if (singleProduct) {
     return (
       <div className="single-card-container">
-        <div className="header">{title}</div>
-        <div>{description}</div>
-        <img src={image} alt={"Slika"} className="image" />
-        <div className="price">Price : {price}$</div>
+        <img src={image} alt={"Slika"} className="single-card-image" />
+        <div className="card-info-container">
+          <div className="single-card-header">{title}</div>
+          <div className="description">{description}</div>
+          <div className="price-single-product">Price : {price}$</div>
+          <NavLink className="btnBack" onClick={() => navigate(-1)}>
+            More products
+          </NavLink>
+        </div>
       </div>
     );
   }
   return (
     <NavLink to={`/product/${id}`} className="card-container">
-      <div className="header">{title}</div>
-      <img src={image} alt={"Slika"} className="image" />
-      <div className="price">Price : {price}$</div>
+      <div className="header-card">{title}</div>
+      <img src={image} alt={"Slika"} className="image-card" />
+      <div className="price-card">Price : {price}$</div>
     </NavLink>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import "./Footer.css";
 
 export const Footer = () => {
-  let navigate = useNavigate();
   return (
     <>
-      <div className="footer-container">
-        <div>Aleksandar Duranovski&copy; 2022</div>
-        <NavLink className="btnBack" onClick={() => navigate(-1)}>
-          Go back
-        </NavLink>
-      </div>
+      <div className="footer-container">Aleksandar Duranovski&copy; 2022</div>
     </>
   );
 };
